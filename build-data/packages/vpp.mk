38e4bdb339de5af1db409eac175ccc52977ac92c
laitianli <laitianli1010@sina.com>
2024-03-13 17:15:52 +0800

    mk: change mk file for separate compilation vpp and dpdk
    first, build dpdk and install dpdk library to directory, such as: ${root_dir}/dpdk/install_dir,
    second, set enviroment DPDK_PATH value: ${root_dir}/dpdk/install_dir
    thrid, run build command: make build/build-release in vpp directory.
    
    Signed-off-by: laitianli <laitianli1010@sina.com>

--- build-data/packages/vpp.mk
+++ build-data/packages/vpp.mk
@@ -35,7 +35,19 @@ ifneq ($(VPP_EXTRA_CMAKE_ARGS),)
 vpp_cmake_args += $(VPP_EXTRA_CMAKE_ARGS)
 endif
 
-vpp_configure_depend += external-install
+#vpp_configure_depend += external-install
+
+prep-dpdk-lib:
+ifneq ($(DPDK_PATH),)
+	@echo "@@@@ Creating libdpdk.a in $(DPDK_PATH)/lib"
+	@cd $(DPDK_PATH)/lib && \
+	 echo "GROUP ( $$(ls librte*.a) )" > libdpdk.a && \
+	 rm -rf librte*.so librte*.so.* dpdk/*/librte*.so dpdk/*/librte*.so.*
+else
+	@echo -e "\033[31m@@@@DPDK Install Path not defined, so building stop! Please set DPDK_PATH enviroment. \033[0m" && exit 1
+endif
+
+vpp_configure_depend += prep-dpdk-lib
 vpp_configure = \
   cd $(PACKAGE_BUILD_DIR) && \
   $(CMAKE) -G Ninja $(vpp_cmake_args) $(call find_source_fn,$(PACKAGE_SOURCE))
